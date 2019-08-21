b52fadffe217eabd0e56bbd5494bafc5bf346c6b
Jeff Chen <hello@jeff.yt>
2019-08-21 10:29:06 -0700

    Initial Commit

---
+++ ya2FA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ya2FA
+//
+//  Created by Jeff Chen on 8/21/19.
+//  Copyright © 2019 Jeff Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
