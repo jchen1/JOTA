1b93fadbd119a61ad2e045c12d936ce23867d474
Jeff Chen <hello@jeff.yt>
2019-08-23 17:21:40 -0700

    .

--- OTPAutoFill/OTPAutoFill.js
+++ OTPAutoFill/OTPAutoFill.js
@@ -4,14 +4,31 @@ OTPAutoFill.prototype = {
     run: function(arguments) {
         arguments.completionFunction({"url": document.URL,
                                      "host": window.location.host
-                                     });
+                                    });
+    },
+    
+    findElement: function(id) {
+        var elementById = document.getElementById(elementById);
+        if (elementById) return elementById;
+        
+        var elementByOTP = document.querySelector("input[autocomplete=\"one-time-code\"]");
+        if (elementByOTP) return elementByOTP;
+        
+        var elementsByInput = document.querySelectorAll("input");
+        if (elementsByInput.length === 1) return elementsByInput[0];
+        
+        return null;
     },
     
     finalize: function(arguments) {
-        var elementId = arguments["id"];
         var code = arguments["code"];
-        var element = document.getElementById(elementId);
-        element.value = code;
+        var element = findElement(arguments["id"]);
+        
+        if (element) {
+            element.value = code;
+        } else {
+            alert("Couldn't find a valid input element!");
+        }
     }
 };
 
